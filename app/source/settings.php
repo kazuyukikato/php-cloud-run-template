bc8b14227f023c22d6a26a31175c257070b463e9
kazuyuki.kato <kazuyuki.kato@standardsystem.co.jp>
2021-09-18 08:07:44 +0900

    add secrets folder

--- app/source/settings.php
+++ app/source/settings.php
@@ -4,10 +4,12 @@
     $bucketname = 'your-bucket-name-here';
 
     // DB関連
+    // local 時は docker-compose.yml で DB_SOCKET_DIR に Unix ソケットパスを記述
     $socketdir = getenv('DB_SOCKET_DIR') ?: '/cloudsql';
     $user = 'test';
     $dbname = 'sample';
     $password = 'test';
+    // Cloud Run 稼働時にはCloud Run の設定で CLOUD_SQL_CONNECTION_NAME を設定
     $connectionName = getenv("CLOUD_SQL_CONNECTION_NAME") ?: 'mysqld.sock';
     
 
